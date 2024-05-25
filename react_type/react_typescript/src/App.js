da52de120c6c21f9d383867148bcb8322ccad119
jungmyeon <zhzsks123@daum.net>
2024-05-25 11:34:02 +0900

    2024-05-25

---
+++ react_type/react_typescript/src/App.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// npx create-react-app 프로젝트명 --template typescript
+// npm install @reduxjs/toolkit redux react-redux
+var react_1 = require("react");
+require("./App.css");
+function App() {
+    return (<div className="App">
+      
+    </div>);
+}
+exports.default = App;
---
+++ ts11.js
@@ -0,0 +1,14 @@
+// npm install -g typescript
+// tsc -w 실행
+// 타입스크립 : 자바스크립트 + 자요형 문법 (타입에러 방지)
+var var11 = 11;
+var number11 = '11';
+// 함수 (매개변수의 자료형, return의 자료형)
+function func11(num, num2) {
+    return 11;
+}
+var 이름11 = { name: '이순신' };
+var _이름 = { name: '이순신', age: 33 };
+// 인터페이스는 이름이 겹치면 합침
+var 아빠 = { name: '신영만', age: 33 };
+var 아들 = { name: '짱구', age: 5 };
